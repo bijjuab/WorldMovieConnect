f99b428318c90ca4d2a764e2b0f98a7ddd1187a4
Bijju <bijju_ab@yahoo.in>
2020-05-09 01:33:34 +0530

    Added tarun.java file

---
+++ EmailApp/src/emailapp/tarun.java
@@ -0,0 +1,11 @@
+package emailapp;
+
+public class tarun {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Tarun Gowtham I.A.S");
+
+	}
+
+}
